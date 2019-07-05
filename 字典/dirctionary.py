b82c7676baa8d501f58850b297e2d214836c8def
key <mail@loong.io>
2019-07-05 23:01:50 +0800

    update

---
+++ 字典/dirctionary.py
@@ -0,0 +1,71 @@
+# 定义一个字典
+person = {'name': "kai", 'age': 26}
+
+# 根据键获取字典的值
+print(person['name'])  # kai
+
+# 遍历字典的键值队
+for key, value in person.items():
+    print(key, value)
+
+"""
+name kai
+age 26
+"""
+
+# 遍历字典的健
+for key in person.keys():
+    print(key)
+
+"""
+name
+age
+"""
+
+# 遍历字典的值
+for value in person.values():
+    print(value)
+
+"""
+kai
+26
+"""
+
+# 添加键值对
+person['height'] = 180
+
+
+# 遍历字典的键值队
+for key, value in person.items():
+    print(key, value)
+
+"""
+name kai
+age 26
+height 180
+"""
+
+# 删除键值对
+del person['height']
+
+# 遍历字典的键值队
+for key, value in person.items():
+    print(key, value)
+
+"""
+name kai
+age 26
+"""
+
+# 修改字典元素
+person['age'] = 25
+
+# 遍历字典的键值队
+for key, value in person.items():
+    print(key, value)
+
+"""
+name kai
+age 25
+"""
+
